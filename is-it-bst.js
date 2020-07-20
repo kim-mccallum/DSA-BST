c104ac3a8686c0a837086492ac6fa50599242425
Kim McCallum <kchaase@gmail.com>
2020-07-19 21:37:08 -0600

    added height of BST

---
+++ is-it-bst.js
@@ -0,0 +1,4 @@
+// Is it a BST?
+// Write an algorithm to check whether an arbitrary binary tree is a binary search tree, assuming the tree does not contain duplicates.
+
+const { BinarySearchTree } = require("./BST-class");
